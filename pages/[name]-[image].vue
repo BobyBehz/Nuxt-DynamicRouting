bc0d5725b5e1e9d3de372a057325b0e1241c7948
babak <babak.behzad79@gmail.com>
2023-08-22 12:36:40 -0700

    done

--- pages/[name]-[image].vue
+++ pages/[name]-[image].vue
@@ -19,16 +19,13 @@ import { ref } from 'vue'
 
 const countryInfo = useRoute().params
 const neighbours = useRoute().query.borders
-console.log(neighbours)
 const borderCountries = ref([])
 const get = async () => {
-    const url = `https://restcountries.com/v3.1/alpha?codes=${neighbours[0]},${neighbours[1]},${neighbours[2]}`
+    const url = `https://restcountries.com/v3.1/alpha?codes=${neighbours}`
     const response = await fetch(url);
     response.json().then(res => {
-        console.log(res)
         borderCountries.value = res
     })
 }
 get()
-console.log(borderCountries.value)
 </script>
